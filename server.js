8c68fe08ef45af8f9f4ff028d0905c78a5bbe561
Naga Sandeep Sadineni <nsadinen@uncc.edu>
2023-09-17 23:10:00 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 const express = require('express');
 const app = express();
-const port = 3040;
-
+const port =3040;
+const data=require('./data.json');
 app.use('/', express.static('public'));
 
-const budget = { 
-    myBudget: [
+const budget ={
+    myBudget:[
     {
-        title: 'Eat Out',
-        budget: 30
+        title: 'Eat out',
+        budget:30
+    
     },
     {
-        title: 'Rent',
-        budget: 350
+        title:'Rent',
+        budget:350
     },
     {
-        title: 'Groceries',
-        budget: 90
-    }
-]};
+        title:'Groceries',
+        budget:90
+    },
+]
+};
+app.get('/hello',(req,res) => {
+    res.send('Hello World!');
 
-app.get('/hello', (req, res) => {
-    res.send("Hello World!");
 });
-
-app.get('/budget', (req, res) => {
-    res.send(budget);
-});
-
+app.get('/budget',(req,res)=>{
+    res.json(data);
+    
+})
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:3040');
+  console.log(`Serving at http://localhost:${port}`)
 });
