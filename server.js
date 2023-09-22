f99f189c5505154a6d8dc66f7a924b7c6e191caf
Naga Sandeep Sadineni <nsadinen@uncc.edu>
2023-09-22 19:28:37 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port =3040;
 const data=require('./data.json');
-app.use('/', express.static('public'));
+
+app.use(cors());
 
 const budget ={
     myBudget:[
